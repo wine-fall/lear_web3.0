5b78b17825319efbd8f51ee30f28d96ada0c0dad
guozhiyi01 <guozhiyi01@baidu.com>
2022-11-04 20:14:04 +0800

    feat: code format

--- first_dapp/src/components/Home/index.tsx
+++ first_dapp/src/components/Home/index.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from 'react';
-import { INFURA_PROJECT_ID } from '../../constant';
+import React, {useEffect} from 'react';
+import {INFURA_PROJECT_ID} from '../../constant';
 
 const Home: React.FC = () => {
     useEffect(() => {
     }, []);
-    return <div>hello home, my INFURA_PROJECT_ID is {INFURA_PROJECT_ID}</div>
-}
+    return <div>hello home, my INFURA_PROJECT_ID is {INFURA_PROJECT_ID}</div>;
+};
 
 export default Home;
